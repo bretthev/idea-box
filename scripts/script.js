51de5be7419be80f7b47a1a550d31238eaedfef2
Andy Young <nxusmg@gmail.com>
2016-07-19 20:31:23 -0600

    Unique ID in article tag, add comments

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,23 @@
-var $titleInput = $('.title-input');  // grab title input
-var $bodyInput = $('.body-input'); //grabs idea body
-var $saveButton = $('.save');
+var $titleInput = $('.title-input');      // call title input field
+var $bodyInput = $('.body-input');        // call body input field
+var $saveButton = $('.save');             // call save btn
 
+// creates container element
 function makeIdeaCard() {
-  $('.idea-list').prepend('<article class="idea-card"><h2>' + $titleInput.val() + '</h2><button class="remove-idea">REMOVE</button><p>' + $bodyInput.val() + '</p><button class="upvote">UPVOTE</button><button class="downvote">DOWNVOTE</button></article>');
+  $('.idea-list').prepend('<article id="'+idGenerator()+'" class="idea-card"><h2>' + $titleInput.val() + '</h2><button class="remove-idea">REMOVE</button><p>' + $bodyInput.val() + '</p><button class="upvote">UPVOTE</button><button class="downvote">DOWNVOTE</button></article>');
 }
 
+// removes container
 function removeParent() {
   $(this).parent().remove();
 }
-
+// calls removeParent when clicked
 $('.idea-list').on('click', '.remove-idea', removeParent);
 
-
+// calls makeIdeaCard which creates element
 $($saveButton).on('click', makeIdeaCard);
+
+// creates unique ID (millisecond time-stamp)
+function idGenerator() {
+  return Date.now().toString();
+}
